b9144ca938f436f750fe89183e1b818c4c642a80
cadupaesleme@gmail.com <cadupaesleme@gmail.com@256911cf-69c8-74a1-9b18-ee176fed643b>
2010-10-15 04:56:27 +0000

    primeiro caso de teste

--- trunk/trabalho_2_PM/src/testes/TestesRotinasPrimalidade.java
+++ trunk/trabalho_2_PM/src/testes/TestesRotinasPrimalidade.java
@@ -1,7 +1,22 @@
 package testes;
 
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import primos.TestePrimos;
+
 
 public class TestesRotinasPrimalidade {
-
+	private TestePrimos numero1 = new TestePrimos(2);
 	
+	@Test
+	public void testForcaBruta(){
+		assertTrue(numero1.calculaForcaBruta());
+		
+		
+		
+	}
 }
