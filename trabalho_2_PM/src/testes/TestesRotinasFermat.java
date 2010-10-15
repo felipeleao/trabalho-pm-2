ddc2b4c90b45582d7348dca80199a551134d0a0a
felipeleao88@gmail.com <felipeleao88@gmail.com@256911cf-69c8-74a1-9b18-ee176fed643b>
2010-10-15 06:00:16 +0000

    Divisão dos testes entre BruteForce e Fermat. Anteriormente todos os testes estavam na unidade de BruteForce. (Versão somente para compreensão do SVN)

--- trabalho_2_PM/src/testes/TestesRotinasFermat.java
+++ trabalho_2_PM/src/testes/TestesRotinasFermat.java
@@ -1,6 +1,19 @@
 package testes;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import primos.TestePrimos;
+
 
 public class TestesRotinasFermat {
-
+private TestePrimos numero1 = new TestePrimos(7);
+	
+	@Test
+	public void testForcaBruta(){
+		assertTrue(numero1.calculaFermat());
+		
+		
+	}
 }
