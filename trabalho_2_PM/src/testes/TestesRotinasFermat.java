20b722fe48aaf9ebe8fc35bc011202c70d54e9ce
cadupaesleme@gmail.com <cadupaesleme@gmail.com@256911cf-69c8-74a1-9b18-ee176fed643b>
2010-10-15 20:40:24 +0000

    criados os casos de teste para os metodos : testFermatNumeroZero() testFermatNumeroCarmichael()

--- trabalho_2_PM/src/testes/TestesRotinasFermat.java
+++ trabalho_2_PM/src/testes/TestesRotinasFermat.java
@@ -92,4 +92,41 @@ public class TestesRotinasFermat {
 	}
 	
 	
+	/**Teste pertencente a classe de equivalencia:
+	 * N é igual a 0
+	 *  
+	 * valores 0
+	 */
+	@Test
+	public void testFermatNumeroZero(){
+		
+		//fazendo set no numero desejado
+		numero.setNumero(0); 
+		assertFalse(numero.calculaFermat());
+		
+	}
+	
+	/**Teste pertencente a classe de equivalencia:
+	 * N é um número de Carmichael
+	 * 
+	 * Obs: o numero de Carmichael é numero primo mas no teste de Fermat é considerado não primo  
+	 *  
+	 * valores 561,1105,1729
+	 */
+	@Test
+	public void testFermatNumeroCarmichael(){
+		
+		//fazendo set no numero desejado
+		numero.setNumero(561); 
+		assertFalse(numero.calculaFermat());
+		
+		
+		//fazendo set no numero desejado
+		numero.setNumero(1105); 
+		assertFalse(numero.calculaFermat());		
+				
+		
+	}
+	
+	
 }
